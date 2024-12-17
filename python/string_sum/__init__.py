40f5ff8c7e8153ecaf40a3df4314ef83cf3fd71b
Clément Walter <clement0walter@gmail.com>
2024-12-17 09:14:05 +0100

    Initial commit

---
+++ python/string_sum/__init__.py
@@ -0,0 +1,6 @@
+from .string_sum import *
+
+
+__doc__ = string_sum.__doc__
+if hasattr(string_sum, "__all__"):
+    __all__ = string_sum.__all__
---
+++ python/tests/test_all.py
@@ -0,0 +1,6 @@
+import pytest
+import string_sum
+
+
+def test_sum_as_string():
+    assert string_sum.sum_as_string(1, 1) == "2"
